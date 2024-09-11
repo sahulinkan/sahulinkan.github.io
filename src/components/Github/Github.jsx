77b6cf3ca0ae50f33c438d038f6393829475f272
Linkan Kumar Sahu <Sahulinkan7@gmail.com>
2024-09-11 18:36:43 +0530

    code updated

--- src/components/Github/Github.jsx
+++ src/components/Github/Github.jsx
@@ -4,7 +4,9 @@ const Github = () => {
   return (
     <>
       <div className="flex justify-center items-center py-5">
-        This is Github page
+        This is Github page. 
+
+        data will be fetched from github api. 
       </div>
     </>
   )

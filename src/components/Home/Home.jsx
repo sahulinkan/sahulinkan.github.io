a786ee33368b05f7f1cb1f4da25a2e706bb5629a
Linkan Kumar Sahu <Sahulinkan7@gmail.com>
2024-09-11 19:43:55 +0530

    updated content

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Home = () => {
   return (
     <>
-      <div className="flex my-5 py-8 justify-evenly bg-white  sm:flex sm:flex-col sm:items-center">
+      <div className="flex my-5 py-8 justify-evenly bg-white">
         <div className="leftbox flex justify-center">
           <img
             src="https://avatars.githubusercontent.com/u/32588495?v=4"
@@ -13,7 +13,7 @@ const Home = () => {
         </div>
         <div className="rightbox flex flex-col justify-start">
           <h1 className="text-5xl text-blue-600 tracking-widest">Hi , <br />I am Linkan Kumar Sahu</h1>
-          <h1 className="text-2xl text-blue-600 tracking-widest text-end">( Data Scientist )</h1>
+          <h1 className="text-2xl text-blue-600 tracking-widest text-end">( Python Developer,ML & NLP Engineer )</h1>
         </div>
       </div>
     </>
